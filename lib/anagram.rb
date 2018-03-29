ee6aa170e95c1c6ef029b52726003dc2c734cfe1
Greg Stimson <stimsong@yahoo.com>
2018-03-29 19:43:36 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -28,17 +28,17 @@ end
 ### compare against sorted given word
 
 ##### FLATIRON SOLUTION: #####
-# 
+#
 # class Anagram
-# 
+#
 #   attr_accessor :name
-# 
+#
 #   def initialize(word)
-#     @name = word 
-#   end   
-# 
+#     @name = word
+#   end
+#
 #   def match(array)
 #     array.select {|x| x.split("").sort == @name.split("").sort}
-#   end   
-# 
+#   end
+#
 # end 
