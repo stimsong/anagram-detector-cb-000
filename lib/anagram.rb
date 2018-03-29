a05453ffb84949bcaae9f2890572ca13c60428fb
Greg Stimson <stimsong@yahoo.com>
2018-03-29 19:40:37 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,28 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @given_word = word.split("")
+  end
+
+  def match(array)
+    anagram_word = []
+    array_word = []
+    index = 0
+
+    array.each do |word|
+      array_word = word.split("")
+      if array_word.sort == @given_word.sort
+        anagram_word << word
+      end
+      index += 1
+    end
+    anagram_word
+  end
+end
+
+### split given word variable into individual letters and sort
+### split each word from given array into individual letters, sort, and
+### compare against sorted given word
